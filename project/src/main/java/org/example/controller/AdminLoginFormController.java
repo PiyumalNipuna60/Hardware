48cef96361c9ee65b4a75027d93842e9d2a1f854
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-23 23:41:11 +0530

    login page implement

---
+++ project/src/main/java/org/example/controller/AdminLoginFormController.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AdminLoginFormController {
+    public TextField txtAdminName;
+    public TextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
