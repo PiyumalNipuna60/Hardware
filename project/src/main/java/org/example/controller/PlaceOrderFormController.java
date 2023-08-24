84baeb7b59029f772566c29c05a0c3135443545b
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-24 23:29:14 +0530

    Add Entity and more

---
+++ project/src/main/java/org/example/controller/PlaceOrderFormController.java
@@ -0,0 +1,26 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class PlaceOrderFormController {
+
+    public TextField txtOrderQuantity;
+    public TextField txtAvailableQuantity;
+    public TextField txtDiscount;
+    public TextField txtProductPrice;
+    public TextField txtNickName;
+    public TextField txtProductName;
+    public TextField txtSearchOnAction;
+    public Pane mainPane;
+
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddProductOnAction(ActionEvent actionEvent) {
+    }
+}
