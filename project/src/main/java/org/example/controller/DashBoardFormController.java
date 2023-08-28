ae8d02be7b07db53b6f7d126178813ea316b45bc
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-28 13:34:30 +0530

    Update table

--- project/src/main/java/org/example/controller/DashBoardFormController.java
+++ project/src/main/java/org/example/controller/DashBoardFormController.java
@@ -14,7 +14,7 @@ public class DashBoardFormController {
     }
 
     public void btnDisplayStockOnAction(ActionEvent actionEvent) {
-        setUi("PlaceOrderForm");
+        setUi("DashBoardForm");
     }
 
     public void btnPlaceInvoiceOnAction(ActionEvent actionEvent) {
