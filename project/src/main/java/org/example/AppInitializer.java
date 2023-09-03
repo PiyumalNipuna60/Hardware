cb450d3e0a1f7b22ac993e6bd509d25e6112923a
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-09-04 01:15:48 +0530

    add OrderDetailsDao, dto, impl class

--- project/src/main/java/org/example/AppInitializer.java
+++ project/src/main/java/org/example/AppInitializer.java
@@ -15,7 +15,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/EmployeeForm.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/PlaceOrderForm.fxml"))));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
